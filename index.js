a61967713506269df66c4658749f859b5eb7b819
Anders Riutta <git@andersriutta.com>
2015-03-22 21:16:06 -0700

    Re-enable tmp fix for waterline validation error

--- index.js
+++ index.js
@@ -183,8 +183,8 @@ var defaults = {
         // TODO when this line is required, it results in
         // an error being thrown when clicking the "Get
         // Token" button on the "Test Auth Flows" page.
-        auth: {model: 'auth', required: true},
-        //auth: {model: 'auth'},
+        //auth: {model: 'auth', required: true},
+        auth: {model: 'auth'},
         status: {type: 'string', required: true}
       }
     }
